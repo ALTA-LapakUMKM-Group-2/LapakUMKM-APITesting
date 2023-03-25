bd50a701c7cc68592819e060d746cce9b58feeca
kukuhpradipto <kukuhpradipto@gmail.com>
2023-03-25 21:05:02 +0700

    editing folder structure JSON, and file API

--- src/test/java/starter/LapakUMKM/DiscussionAPI.java
+++ src/test/java/starter/LapakUMKM/DiscussionAPI.java
@@ -2,21 +2,28 @@ package starter.LapakUMKM;
 import com.github.javafaker.Faker;
 import net.serenitybdd.rest.SerenityRest;
 import net.thucydides.core.annotations.Step;
+import org.checkerframework.checker.units.qual.C;
 import starter.LapakUMKM.Utils.ConstantDiscussion;
 
 
 public class DiscussionAPI {
 
     //POST
+    public static String POST_CREATE_DISCUSSION = ConstantDiscussion.BASE_URL + "discussion";
 
 
     //GET
+    public static String GET_DISCUSSION = ConstantDiscussion.BASE_URL + "discussion/{id}";
 
 
     //PUT
+    public static String PUT_UPDATE_DISCUSSION = ConstantDiscussion.BASE_URL + "discussion/{id}";
+
 
 
     //DELETE
+    public static String DELETE_DISCUSSION = ConstantDiscussion.BASE_URL + "discussion/{id}";
+
 
 
 }
