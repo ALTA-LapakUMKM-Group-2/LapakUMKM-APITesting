d22363b5b95f4ac16b36cedd3f0070b4cbefe9a9
kukuhpradipto <kukuhpradipto@gmail.com>
2023-03-29 06:33:21 +0700

    add put on feedback feature

--- src/test/java/starter/LapakUMKM/StepDefinitions/FeatureFeedback/PUTFeedbackStepdefs.java
+++ src/test/java/starter/LapakUMKM/StepDefinitions/FeatureFeedback/PUTFeedbackStepdefs.java
@@ -32,4 +32,58 @@ public class PUTFeedbackStepdefs {
         File jsonSchema = new File(ConstantFeedback.JSON_SCHEMA_PUT + "/PutJsonSchema.json");
         SerenityRest.then().assertThat().body(JsonSchemaValidator.matchesJsonSchema(jsonSchema));
     }
+
+    @And("Validate put invalid update data resource json schema")
+    public void validatePutInvalidUpdateDataResourceJsonSchema() {
+        File jsonSchema = new File(ConstantFeedback.JSON_SCHEMA_PUT + "/PutInvalidJsonSchema.json");
+        SerenityRest.then().assertThat().body(JsonSchemaValidator.matchesJsonSchema(jsonSchema));
+    }
+
+    @Given("Put update data request body feedback id {int}")
+    public void putUpdateDataRequestBodyFeedbackId(int id) {
+        File jsonReq = new File(ConstantFeedback.JSON_REQ_BODY_PUT + "/PutFeedbackIDReqBody.json");
+        feedbackAPI.setPutUpdateFeedback(id, jsonReq);
+    }
+
+    @Given("Put update data request body product id {int}")
+    public void putUpdateDataRequestBodyProductId(int id) {
+        File jsonReq = new File(ConstantFeedback.JSON_REQ_BODY_PUT + "/PutProductIDFeedbackReqBody.json");
+        feedbackAPI.setPutUpdateFeedback(id, jsonReq);
+    }
+
+    @Given("Put update data request body invalid product id {int}")
+    public void putUpdateDataRequestBodyInvalidProductId(int id) {
+        File jsonReq = new File(ConstantFeedback.JSON_REQ_BODY_PUT + "/PutInvalidProductIDFeedbackReqBody.json");
+        feedbackAPI.setPutUpdateFeedback(id, jsonReq);
+    }
+
+    @Given("Put update data with request body product id is blank id {int}")
+    public void putUpdateDataWithRequestBodyProductIdIsBlankId(int id) {
+        File jsonReq = new File(ConstantFeedback.JSON_REQ_BODY_PUT + "/PutProductIDBlankFeedbackReqBody.json");
+        feedbackAPI.setPutUpdateFeedback(id, jsonReq);
+    }
+
+    @Given("Put Create new data with request body rating is blank id {int}")
+    public void putCreateNewDataWithRequestBodyRatingIsBlankId(int id) {
+        File jsonReq = new File(ConstantFeedback.JSON_REQ_BODY_PUT + "/PutRatingBlankFeedbackReqBody.json");
+        feedbackAPI.setPutUpdateFeedback(id, jsonReq);
+    }
+
+    @Given("Put Create new data with request body invalid rating id {int}")
+    public void putCreateNewDataWithRequestBodyInvalidRatingId(int id) {
+        File jsonReq = new File(ConstantFeedback.JSON_REQ_BODY_PUT + "/PutRatingInvalidFeedbackReqBody.json");
+        feedbackAPI.setPutUpdateFeedback(id, jsonReq);
+    }
+
+    @Given("Given Put update data request body with valid json {int}")
+    public void givenPutUpdateDataRequestBodyWithValidJson(int id) {
+        File jsonReq = new File(ConstantFeedback.JSON_REQ_BODY_PUT + "/PutValidFeedbackReqBody.json");
+        feedbackAPI.setPutUpdateFeedback(id, jsonReq);
+    }
+
+    @Given("Put update data with feedback id is blank id {int}")
+    public void putUpdateDataWithFeedbackIdIsBlankId(int id) {
+        File jsonReq = new File(ConstantFeedback.JSON_REQ_BODY_PUT + "/PutFeedbackBlankReqBody.json");
+        feedbackAPI.setPutUpdateFeedback(id, jsonReq);
+    }
 }
--- src/test/resources/features/FetureFeedback/PUTFeedback.feature
+++ src/test/resources/features/FetureFeedback/PUTFeedback.feature
@@ -1,8 +1,80 @@
 Feature: PUT Update data Feedback
 
-  @ProjectTeam4 @PositiveCase
+  @Team2 @Feedback @Positive
   Scenario: Put update data with valid json
-    Given Put update data request body with valid json id 24
+    Given Given Put update data request body with valid json 24
     When Send put update data
     Then Status code should be 200 OK
     And Validate put update data resource json schema
+
+
+  @Team2 @Feedback @Positive
+  Scenario: Put update data feedback
+    Given Put update data request body feedback id 24
+    When Send put update data
+    Then Status code should be 200 OK
+    And Validate put update data resource json schema
+
+
+  @Team2 @Feedback @Positive
+  Scenario: Put update data product id
+    Given Put update data request body product id 24
+    When Send put update data
+    Then Status code should be 200 OK
+    And Validate put update data resource json schema
+
+
+  @Team2 @Feedback @Negative
+  Scenario: Put update data invalid product id
+    Given Put update data request body invalid product id 24
+    When Send put update data
+    Then Status code should be 404 Not Found
+    And Validate put invalid update data resource json schema
+
+
+  @Team2 @Feedback @Negative
+  Scenario: Put update data with product id is blank
+    Given Put update data with request body product id is blank id 24
+    When Send put update data
+    Then Status code should be 404 Not Found
+    And Validate put invalid update data resource json schema
+
+
+  @Team2 @Feedback @Negative
+  Scenario: Put update data with product id is blank
+    Given Put update data with request body product id is blank id 24
+    When Send put update data
+    Then Status code should be 404 Not Found
+    And Validate put invalid update data resource json schema
+
+
+  @Team2 @Feedback @Negative
+  Scenario: Put update data with feedback id is blank
+    Given Put update data with request body product id is blank id 24
+    When Send put update data
+    Then Status code should be 404 Not Found
+    And Validate put invalid update data resource json schema
+
+
+  @Team2 @Feedback @Negative
+  Scenario: Put Create new data with request body rating is blank
+    Given Put Create new data with request body rating is blank id 24
+    When Send put update data
+    Then Status code should be 400 Bad Request
+    And Validate put invalid update data resource json schema
+
+
+  @Team2 @Feedback @Negative
+  Scenario: Put Create new data with request body invalid rating
+    Given Put Create new data with request body invalid rating id 24
+    When Send put update data
+    Then Status code should be 400 Bad Request
+    And Validate put invalid update data resource json schema
+
+
+  @Team2 @Feedback @Negative
+  Scenario: Put update data with feedback id is blank
+    Given Put update data with feedback id is blank id 24
+    When Send put update data
+    Then Status code should be 404 Not Found
+    And Validate put invalid update data resource json schema
